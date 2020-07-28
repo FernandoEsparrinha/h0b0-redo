3be8f5579b5b08a6ba98f9b7362cb45e7baaffca
FernandoPinto <fernando.esparrinha@gmail.com>
2020-07-28 20:27:10 +0100

    Clean redo.js

--- redo.js
+++ redo.js
@@ -1,50 +1,31 @@
 p5.disableFriendlyErrors = true;
 
+let polygon;
+
 function preload() {
     // song = loadSound('assets/SocialConstruct.mp3')
     // song = loadSound('assets/TameImpala.mp3')
+
+    // loadVideo
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight)
+    createCanvas(windowWidth, windowHeight);
+
+    polygon = new Polygon(0, 0, 400, 18);
 }
 
 function draw() {
+    clear()
     noFill()
-    stroke('teal')
-    strokeWeight(0.1)
-
-
-    translate(windowWidth / 2, windowHeight / 2)
-    scale(8, 8);
-    // translate(windowWidth / 2, windowHeight / 4)
-    // scale(12, 12);
-    drawPolygon(18);
+    strokeWeight(1)
+    polygon.draw();
 }
 
-function drawPolygon(n) {
-    ang = ((180 * (n - 2)) / n) * (Math.PI / 180)
-    alt = Math.tan(ang / 2) * 10
-
-    translate(-10, alt)
-
-    v0 = [0, 0]
-    v1 = [20, 0]
-
-    // beginShape()
-    // vertex(v0[0], v0[1])
-    // vertex(v1[0], v1[1])
-
-    circle(v0[0], v0[1], 10);
-    circle(v1[0], v1[1], 10);
-    for (i = 0; i < n - 2; i++) {
-        x = v1[0] + (v0[0] - v1[0]) * Math.cos(ang) - (v0[1] - v1[1]) * Math.sin(ang)
-        y = v1[1] + (v0[0] - v1[0]) * Math.sin(ang) + (v0[1] - v1[1]) * Math.cos(ang)
-
-        // vertex(x, y)
-        circle(x, y, 10);
-        v0 = v1
-        v1 = [x, y]
+function keyPressed() {
+    if (keyCode === 37) {
+        polygon.next()
+    } else if (keyCode === 39) {
+        polygon.previous()
     }
-    // endShape(CLOSE)
 }
