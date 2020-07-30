19a2556ba4f89492f70bfa628a2df89f547c5a03
FernandoPinto <fernando.esparrinha@gmail.com>
2020-07-30 12:50:21 +0100

    Implement Loading message + music control

--- redo.js
+++ redo.js
@@ -1,31 +1,44 @@
 p5.disableFriendlyErrors = true;
 
-let polygon;
-
-function preload() {
-    // song = loadSound('assets/SocialConstruct.mp3')
-    // song = loadSound('assets/TameImpala.mp3')
-
-    // loadVideo
-}
+let polygon, musicController;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight)
 
-    polygon = new Polygon(0, 0, 400, 18);
+    // loadTracklist()
+    musicController = new MusicController();
+    polygon = new Polygon(0, 0, 400, 18)
 }
 
 function draw() {
     clear()
     noFill()
     strokeWeight(1)
-    polygon.draw();
+    if (tracksLoaded) {
+        polygon.draw()
+    } else {
+        fill(0, 102, 153)
+        textSize(32)
+        text('Loading (' + (loadIndex + 1) + '/18)', windowWidth / 2, windowHeight / 2)
+    }
 }
 
 function keyPressed() {
     if (keyCode === 37) {
-        polygon.next()
+        // polygon.next()
+        musicController.next()
+
     } else if (keyCode === 39) {
-        polygon.previous()
+        // polygon.previous()
+        musicController.previous()
     }
+}
+
+function mousePressed() {
+    print(mouseX)
+    print(mouseY)
+    // let d = dist(mouseX, mouseY, circleCenterX, circleCenterY)
+    // if (d < circleSize) {
+    //   active = !active;
+    // }
 }
